0a5fc62c12d0a77cc8696c336e2f9230c23e8a3e
Nikhila <atnikhila@gmail.com>
2025-11-23 13:25:22 +0530

    node application

--- .github/workflows/test/test.js
+++
@@ -1,8 +0,0 @@
-const sum= require('../index')
-test('Test case 1: adds 2+3 equals to 5',()=>{
-    expect(sum(2,3)).toBe(5)
-})
-
-test('Test case 2: add negative numbers ', ()=>{
-    expect(sum(-2,-3)).toBe(-5)
-})
